27a19a722c91888265f2668d930e9a4ed25991b4
Gabriel Dezzani Pitzer <gabrielpitzer@hotmail.com>
2024-05-06 21:54:15 -0300

    consertando bug

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando"/>';
 const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado"/>';
 const atividades = []; // cria um array vazio
 const notas = [];
-const spanAprovado = '<span class="resultado aprovado">Aprovado/>'
-const spanReprovado = '<span class="resultado reprovado">Reprovado/>'
+const spanAprovado = '<span class="resultado aprovado">Aprovado'
+const spanReprovado = '<span class="resultado reprovado">Reprovado'
+const notaMinima = parseFloat(prompt("Digite a nota mínima: "));
 
 
 let linhas  = ''; // faz com que a tabela receba mais de uma linha 
@@ -21,20 +22,24 @@ function adicionaLinha() { //tem a função de adicionar linha nova à variável
     const inputNomeAtividade = document.getElementById('nome-atividade'); // cria uma constantes para os ids
     const inputNotaAtividade = document.getElementById('nota-atividade');
 
+    if(atividades.includes(inputNomeAtividade.value)) {
+        alert(`A atividade: ${inputNomeAtividade.value} já foi inserida`)
+    } else{
+
     atividades.push(inputNomeAtividade.value); // joga o valor fornecido pelo usuario na array vazia criada para no final gerar a média.
     notas.push(parseFloat(inputNotaAtividade.value)); // define que o valor será um numero e nao uma string 
 
     let linha = '<tr>';
     linha += `<td>${inputNomeAtividade.value}</td>`;
     linha += `<td>${inputNotaAtividade.value}</td>`;
-    linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`;  //? é o if e : é o else
+    linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`;  //? é o if e : é o else
     linha += `</tr>`;
 
     linhas += linha;
 }
     inputNomeAtividade.value = '';
-    inputNotaAtividade.value = '';  // limpa os campos apo o uso
-
+    inputNotaAtividade.value = '';  // limpa os campos apos o uso
+}
 function atualizaTabela(){
     const corpoTabela = document.querySelector('tbody'); //substitui o tbody pelo que foi escrito no js 
     corpoTabela.innerHTML = linhas;
@@ -44,14 +49,14 @@ function atualizaMediaFinal() {
     const mediaFinal = calculaMediaFinal();
 
     document.getElementById('media-final-valor').innerHTML = mediaFinal
-    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= 7 ? spanAprovado : spanReprovado;
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 
 }
 
 function calculaMediaFinal() {
     let somaDasNotas = 0;
 
-    for (let i = 0; i < notas.length; i++);{
+    for (let i = 0; i < notas.length; i++){
         somaDasNotas += notas[i];
     }
 
